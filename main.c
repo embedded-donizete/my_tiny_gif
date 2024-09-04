ade359c288b281c1f1390fe95be111c709100288
DonizeteVida <donizetevida1998@outlook.com>
2024-09-03 21:44:16 -0300

    Initial project files

---
+++ main.c
@@ -0,0 +1,7 @@
+#include <my_tiny_gif.h>
+
+int main(int argc, char const *argv[])
+{
+    say_hello();
+    return 0;
+}
---
+++ my_tiny_gif.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <my_tiny_gif.h>
+
+void say_hello()
+{
+    printf("Hello, from my_tiny_gif!\n");
+}
